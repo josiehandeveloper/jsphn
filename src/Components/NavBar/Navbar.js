82cd085aed11251e7fd23cacef63ffe21aae6811
Josephine Han <jsphnhan@gmail.com>
2021-04-30 17:16:17 -0700

    navbar className changed from - to __

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -10,31 +10,31 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <a href="#home" className="navbar-logo">
+        <a href="#home" className="navbar__logo">
           JSPHN
         </a>
-        <div className="menu-icon" onClick={this.handleClick}>
+        <div className="menu__icon" onClick={this.handleClick}>
           <i
             className={this.state.clicked ? "fal fa-times" : "fal fa-equals"}
           ></i>
         </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-links">
+        <ul className={this.state.clicked ? "nav__menu active" : "nav__menu"}>
+          <li className="nav__links">
             <a href="#about" className="about">
               ABOUT
             </a>
           </li>
-          <li className="nav-links">
+          <li className="nav__links">
             <a href="#services" className="services">
               SERVICES
             </a>
           </li>
-          <li className="nav-links">
+          <li className="nav__links">
             <a href="#portfolio" className="portfolio">
               PORTFOLIO
             </a>
           </li>
-          <li className="nav-links">
+          <li className="nav__links">
             <a href="#contact" className="contact">
               CONTACT
             </a>
