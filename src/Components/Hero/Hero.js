fb587aaed5d57001aa35b8db2519e6825aca46fb
Josephine Han <jsphnhan@gmail.com>
2021-04-30 23:02:39 -0700

    hero-section text adjust

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,29 +1,19 @@
 import React from "react";
 import dahlia from "../../images/dahlia.jpg";
-import scroll from "../../images/scroll.png";
 import "./Hero.css";
 
 function Hero() {
   return (
-    <div className="hero-container">
-      <div className="hero-wrapper">
-        <div className="hero-item-1">
+    <div className="hero__container">
+      <div className="hero__wrapper">
+        <div className="hero__item__1">
           <h1>HELLO</h1>
+        </div>
+        <div className="hero__item__2">
           <h2>I'M JOSEPHINE</h2>
         </div>
-        <div className="hero-items">
-          <div className="hero-item-2">
-            <img
-              src={scroll}
-              alt="scroll"
-              className="scroll"
-              width="50px"
-              height="200px"
-            />
-          </div>
-          <div className="hero-item-3">
-            <img src={dahlia} alt="dahlia" className="dahlia" width="500px" />
-          </div>
+        <div className="hero__item__3">
+          <img src={dahlia} alt="dahlia" className="dahlia" width="500px" />
         </div>
       </div>
     </div>
