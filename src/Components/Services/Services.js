67dfff2ae5ce286a5bb862c19c8ed54694ea2d7f
Josephine Han <jsphnhan@gmail.com>
2021-05-03 22:31:39 -0700

    services fixed

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -3,27 +3,27 @@ import "./Services.css";
 
 function Services() {
   return (
-    <div className="services__container">
-      <div className="services__wrapper">
-        <div className="services-left">
-          <div className="services-left-bottom"></div>
-        </div>
-        <div className="services-right">
-          <h1>Services</h1>
-          <ul>
-            <li>
-              <h3>Functionality</h3>
-              <p>Develop a complete and functional website</p>
-            </li>
-            <li>
-              <h3>Aesthetics</h3>
-              <p>Focus on great design</p>
-            </li>
-            <li>
-              <h3>User Experience</h3>
-              <p>Incorporate aspects of user interface and user experience</p>
-            </li>
-          </ul>
+    <div className="services">
+      <div className="services__container">
+        <div className="services__left__pic"></div>
+        <div className="services__right">
+          <div className="services__right__content">
+            <h1>Services</h1>
+            <ul className="services__list">
+              <li>
+                <h3>Functionality</h3>
+                <p>Develop a complete and functional website</p>
+              </li>
+              <li>
+                <h3>Aesthetics</h3>
+                <p>Focus on great design</p>
+              </li>
+              <li>
+                <h3>User Experience</h3>
+                <p>Incorporate aspects of user interface and user experience</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
