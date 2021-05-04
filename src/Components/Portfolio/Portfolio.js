a99632e6a0ad3a709bf19dc0bee13e8b414f97d0
Josephine Han <jsphnhan@gmail.com>
2021-05-04 12:25:43 -0700

    hero photo edit

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -13,19 +13,16 @@ function Portfolio() {
               src="images/UrbanGif/search-macbook.png"
               text="UrbanGifs is a fun application where users search within the UrbanDictionary database and the GIPHY database mashing both results onto a search page! This application is for those who like GIFS and Urban Dictionary, old and young users alike will find this search bar as a fun and dynamic way to search for words and GIFS."
               title="UrbanGif"
-              path="/portfolio"
             />
             <PortfolioItem
               src="images/WatchIt/watchIt1.png"
               text="Watch It is an application where users can login, search for movies within the Open Movie Database, and create a customized movie query. With more indoor time I was inspired to create this application so users can find new movies to watch that might not be available on certain streaming sites."
               title="WatchIt"
-              path="/portfolio"
             />
             <PortfolioItem
               src="images/Grateful/grateful1.png"
               text="Grateful is an application where users can log and practice their gratitude. This application is for everyone who wants to practice mindfulness. This pandemic has put a mental strain on all of us and it's important to recognize the toll it has taken on us and promote practices to achieve a better state of mental health."
               title="Grateful"
-              path="/portfolio"
             />
           </ul>
         </div>
