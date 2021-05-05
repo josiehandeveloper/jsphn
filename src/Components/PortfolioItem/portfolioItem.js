33f2f4f8d4cc269d6872fb7df645cd818b924552
Josephine Han <jsphnhan@gmail.com>
2021-05-04 18:32:08 -0700

    removed link from portfolioItem

--- src/Components/PortfolioItem/portfolioItem.js
+++ src/Components/PortfolioItem/portfolioItem.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function PortfolioItem(props) {
   return (
     <>
       <li className="portfolio__item">
-        <Link className="portfolio__item__link" to={props.path}>
+        <div className="portfolio__item__link">
           <div className="portfolio__item__pic-wrap">
             <img
               className="portfolio__item__img"
@@ -17,7 +16,7 @@ function PortfolioItem(props) {
             <h4 className="portfolio__item__title">{props.title}</h4>
             <h5 className="portfolio__item__text">{props.text}</h5>
           </div>
-        </Link>
+        </div>
       </li>
     </>
   );
