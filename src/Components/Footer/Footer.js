2b1ee9f5d62b8fef8c74a4911cb3fb80adc9aebf
Josephine Han <jsphnhan@gmail.com>
2021-05-06 14:06:37 -0700

    _target added to footer links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,13 +5,28 @@ const Footer = () => {
   return (
     <div id="contact" className="footer-container">
       <div className="footer-wrap">
-        <a className="link" href="mailto:jsphnhan@gmail.com">
+        <a
+          className="link"
+          href="mailto:jsphnhan@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fas fa-envelope-square"></i>
         </a>{" "}
-        <a className="link" href="https://github.com/josiehandeveloper">
+        <a
+          className="link"
+          href="https://github.com/josiehandeveloper"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fab fa-github"></i>
         </a>{" "}
-        <a className="link" href="https://www.linkedin.com/in/josephine-han/">
+        <a
+          className="link"
+          href="https://www.linkedin.com/in/josephine-han/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fab fa-linkedin-in"></i>
         </a>
       </div>
