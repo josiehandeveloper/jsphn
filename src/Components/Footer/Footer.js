16b79222b618a5147e989eeb0b60b1e0ab16c17c
Josephine Han <jsphnhan@gmail.com>
2021-06-01 14:56:46 -0700

    new resume added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -31,7 +31,7 @@ const Footer = () => {
         </a>
         <a
           className="link"
-          href="https://drive.google.com/file/d/1Br5xE3K_qKIy4SWjb3htBzk6jWtUuZpJ/view?usp=sharing"
+          href="https://docs.google.com/document/d/1aSMDBButRYQEdWI7NGBPORGpolytGyysqtswP2vPVoQ/export?format=pdf"
           target="_blank"
           rel="noreferrer"
         >
