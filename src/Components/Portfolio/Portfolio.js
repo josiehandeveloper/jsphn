2b01de81ecad458605091d52e25925990d72867f
Josephine Han <jsphnhan@gmail.com>
2021-06-12 19:07:38 -0700

    app info passed through props

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PortfolioItem from "../PortfolioItem/PortfolioItem";
 import "./Portfolio.css";
 
 function Portfolio() {
@@ -8,201 +9,47 @@ function Portfolio() {
       <div className="portfolio__container">
         <div className="portfolio__wrapper">
           <ul className="portfolio__items">
-            <li className="portfolio__item">
-              <div className="portfolio__item__box">
-                <div className="portfolio__item__pic-wrap">
-                  <img
-                    className="portfolio__item__img"
-                    alt="Portfolio img"
-                    src="images/UrbanGif/search-macbook.png"
-                  />
-                </div>
-                <div className="portfolio__item__info">
-                  <h4 className="portfolio__item__title">UrbanGif</h4>
-                  <h4 className="portfolio__item__links">
-                    <a
-                      href="https://josiehandeveloper.github.io/urban-gifs/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Site
-                    </a>
-                    <a
-                      href="https://github.com/josiehandeveloper/urban-gifs.git"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Code
-                    </a>
-                  </h4>
-                  <h5 className="portfolio__item__text">
-                    UrbanGifs is a fun application where users search within the
-                    UrbanDictionary database and the GIPHY database mashing both
-                    results onto a search page! This application is for those
-                    who like GIFS and Urban Dictionary, old and young users
-                    alike will find this search bar as a fun and dynamic way to
-                    search for words and GIFS.
-                  </h5>
-                  <ul className="portfolio__item__tech">
-                    {" "}
-                    <h3>Technologies Used:</h3>
-                    <li>JavaScript ES6</li>
-                    <li>jQuery</li>
-                    <li>CSS3</li>
-                    <li>HTML 5</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-            <li className="portfolio__item">
-              <div className="portfolio__item__box">
-                <div className="portfolio__item__pic-wrap">
-                  <img
-                    className="portfolio__item__img"
-                    alt="Portfolio img"
-                    src="images/WatchIt/watchIt1.png"
-                  />
-                </div>
-                <div className="portfolio__item__info">
-                  <h4 className="portfolio__item__title">WatchIt</h4>
-                  <h4 className="portfolio__item__links">
-                    <a
-                      href="https://watchit-app-seven.vercel.app/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Site
-                    </a>
-                    <a
-                      href="https://github.com/josiehandeveloper/watchit-app.git"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Client Side
-                    </a>
-                    <a
-                      href="https://github.com/josiehandeveloper/watchit-2-server.git"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Server Side
-                    </a>
-                  </h4>
-                  <h5 className="portfolio__item__text">
-                    Watch It is an application where users can login, search for
+            <PortfolioItem
+              src="images/WatchIt/watchIt1.png"
+              title="WatchIt"
+              live="https://watchit-app-seven.vercel.app/"
+              client="https://github.com/josiehandeveloper/watchit-app.git"
+              server="https://github.com/josiehandeveloper/watchit-2-server.git"
+              text="Watch It is an application where users can login, search for
                     movies within the Open Movie Database, and create a
                     customized movie query. With more indoor time I was inspired
                     to create this application so users can find new movies to
                     watch that might not be available on certain streaming
-                    sites.
-                  </h5>
-                  <ul className="portfolio__item__tech">
-                    {" "}
-                    <h3>Technologies Used:</h3>
-                    <li>React.js</li>
-                    <li>CSS3</li>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>PostgreSQL</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <ul className="portfolio__items">
-            <li className="portfolio__item">
-              <div className="portfolio__item__box">
-                <div className="portfolio__item__pic-wrap">
-                  <img
-                    className="portfolio__item__img"
-                    alt="Portfolio img"
-                    src="images/Grateful/grateful1.png"
-                  />
-                </div>
-                <div className="portfolio__item__info">
-                  <h4 className="portfolio__item__title">Grateful</h4>
-                  <h4 className="portfolio__item__links">
-                    <a
-                      href="https://grateful-app.vercel.app/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Site
-                    </a>
-                    <a
-                      href="https://github.com/josiehandeveloper/grateful-app.git"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Client Side
-                    </a>
-                    <a
-                      href="https://github.com/josiehandeveloper/grateful2-server.git"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Server Side
-                    </a>
-                  </h4>
-                  <h5 className="portfolio__item__text">
-                    Grateful is an application where users can log and practice
+                    sites."
+              tech="React.js, CSS3, Node.js, Express, PostgreSQL"
+            />
+            <PortfolioItem
+              src="images/Grateful/grateful1.png"
+              title="Grateful"
+              live="https://grateful-app.vercel.app/"
+              client="https://github.com/josiehandeveloper/grateful-app.git"
+              server="https://github.com/josiehandeveloper/grateful2-server.git"
+              text="Grateful is an application where users can log and practice
                     their gratitude. This application is for everyone who wants
                     to practice mindfulness. This pandemic has put a mental
                     strain on all of us and it's important to recognize the toll
                     it has taken on us and promote practices to achieve a better
-                    state of mental health.
-                  </h5>
-                  <ul className="portfolio__item__tech">
-                    {" "}
-                    <h3>Technologies Used:</h3>
-                    <li>React.js</li>
-                    <li>CSS3</li>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>PostgreSQL</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-            <li className="portfolio__item">
-              <div className="portfolio__item__box">
-                <div className="portfolio__item__pic-wrap">
-                  <img
-                    className="portfolio__item__img"
-                    alt="Portfolio img"
-                    src="images/LeQuotidian/le-quotidian.png"
-                  />
-                </div>
-                <div className="portfolio__item__info">
-                  <h4 className="portfolio__item__title">Le Quotidian</h4>
-                  <h4 className="portfolio__item__links">
-                    <a
-                      href="https://le-quotidian.vercel.app/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Site
-                    </a>
-                  </h4>
-                  <h5 className="portfolio__item__text">
-                    Le Quotidian is a mock ecommerce store selling environmental
-                    friendly products. It uses Commerce.js and Stripe to create
-                    a fully functional ecommerce site. The front end was
-                    designed with Material UI and the shipping form was created
-                    with React Hook Form.
-                  </h5>
-                  <ul className="portfolio__item__tech">
-                    {" "}
-                    <h3>Technologies Used:</h3>
-                    <li>React.js</li>
-                    <li>Commerce.js</li>
-                    <li>React Hook Form</li>
-                    <li>Material UI</li>
-                    <li>Stripe</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
+                    state of mental health."
+              tech="React.js, CSS3, Node.js, Express, PostgreSQL"
+            />
+            <PortfolioItem
+              src="images/UrbanGif/search-macbook.png"
+              title="UrbanGif"
+              live="https://josiehandeveloper.github.io/urban-gifs/"
+              client="https://github.com/josiehandeveloper/urban-gifs.git"
+              text="UrbanGifs is a fun application where users search within the
+                    UrbanDictionary database and the GIPHY database mashing both
+                    results onto a search page! This application is for those
+                    who like GIFS and Urban Dictionary, old and young users
+                    alike will find this search bar as a fun and dynamic way to
+                    search for words and GIFS."
+              tech="JavaScript ES6, jQuery, CSS3, HTML 5"
+            />
           </ul>
         </div>
       </div>
--- src/Components/PortfolioItem/portfolioItem.js
+++ src/Components/PortfolioItem/portfolioItem.js
@@ -1,28 +1,44 @@
 import React from "react";
+import "./PortfolioItem.css";
 
 function PortfolioItem(props) {
   return (
-    <>
-      <li className="portfolio__item">
-        <div className="portfolio__item__link">
-          <div className="portfolio__item__pic-wrap">
-            <img
-              className="portfolio__item__img"
-              alt="Portfolio img"
-              src={props.src}
-            />
-          </div>
-          <div className="portfolio__item__info">
-            <h4 className="portfolio__item__title">{props.title}</h4>
-            <h4 className="portfolio__item__links">
-              {props.live}
-              {props.code}
-            </h4>
-            <h5 className="portfolio__item__text">{props.text}</h5>
-          </div>
-        </div>
-      </li>
-    </>
+    <div className="portfolio__container">
+      <div className="portfolio__wrapper">
+        <ul className="portfolio__items">
+          <li className="portfolio__item">
+            <div className="portfolio__item__box">
+              <div className="portfolio__item__pic-wrap">
+                <img
+                  className="portfolio__item__img"
+                  alt="Portfolio img"
+                  src={props.src}
+                />
+              </div>
+              <div className="portfolio__item__info">
+                <h4 className="portfolio__item__title">{props.title}</h4>
+                <h4 className="portfolio__item__links">
+                  <a href={props.live} target="_blank" rel="noreferrer">
+                    Live Site
+                  </a>
+                  <a href={props.client} target="_blank" rel="noreferrer">
+                    Client
+                  </a>
+                  <a href={props.server} target="_blank" rel="noreferrer">
+                    Server
+                  </a>
+                </h4>
+                <h5 className="portfolio__item__text">{props.text}</h5>
+                <h5 className="portfolio__item__tech">
+                  {" "}
+                  Technologies Used: {props.tech}
+                </h5>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
