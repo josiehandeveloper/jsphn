e320d924e263527bc1e4dff9ecc5e87047e45281
Josephine Han <jsphnhan@gmail.com>
2021-06-17 19:06:58 -0700

    glampers added

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -7,51 +7,58 @@ function Portfolio() {
     <div id="portfolio" className="portfolio">
       <h1>Portfolio</h1>
       <div className="portfolio__container">
-        <div className="portfolio__wrapper">
-          <ul className="portfolio__items">
-            <PortfolioItem
-              src="images/WatchIt/watchIt1.png"
-              title="WatchIt"
-              live="https://watchit-app-seven.vercel.app/"
-              client="https://github.com/josiehandeveloper/watchit-app.git"
-              server="https://github.com/josiehandeveloper/watchit-2-server.git"
-              text="Watch It is an application where users can login, search for
+        <PortfolioItem
+          src="images/Glampers/glampers.png"
+          title="Glampers"
+          live="https://glampers.vercel.app/"
+          client="https://github.com/josiehandeveloper/glampers"
+          server="https://github.com/josiehandeveloper/glampers-server"
+          text="Glampers is an Airbnb inspired app where users can look for their one of a kind stay and add it to their cart"
+          tech="React.js, CSS3, Node.js, Express, PostgreSQL, Material-UI"
+        />
+        <PortfolioItem
+          src="images/WatchIt/watchIt1.png"
+          title="WatchIt"
+          live="https://watchit-app-seven.vercel.app/"
+          client="https://github.com/josiehandeveloper/watchit-app.git"
+          server="https://github.com/josiehandeveloper/watchit-2-server.git"
+          text="Watch It is an application where users can login, search for
                     movies within the Open Movie Database, and create a
                     customized movie query. With more indoor time I was inspired
                     to create this application so users can find new movies to
                     watch that might not be available on certain streaming
                     sites."
-              tech="React.js, CSS3, Node.js, Express, PostgreSQL"
-            />
-            <PortfolioItem
-              src="images/Grateful/grateful1.png"
-              title="Grateful"
-              live="https://grateful-app.vercel.app/"
-              client="https://github.com/josiehandeveloper/grateful-app.git"
-              server="https://github.com/josiehandeveloper/grateful2-server.git"
-              text="Grateful is an application where users can log and practice
+          tech="React.js, CSS3, Node.js, Express, PostgreSQL"
+        />
+      </div>
+      <div className="portfolio__container">
+        <PortfolioItem
+          src="images/Grateful/grateful1.png"
+          title="Grateful"
+          live="https://grateful-app.vercel.app/"
+          client="https://github.com/josiehandeveloper/grateful-app.git"
+          server="https://github.com/josiehandeveloper/grateful2-server.git"
+          text="Grateful is an application where users can log and practice
                     their gratitude. This application is for everyone who wants
                     to practice mindfulness. This pandemic has put a mental
                     strain on all of us and it's important to recognize the toll
                     it has taken on us and promote practices to achieve a better
                     state of mental health."
-              tech="React.js, CSS3, Node.js, Express, PostgreSQL"
-            />
-            <PortfolioItem
-              src="images/UrbanGif/search-macbook.png"
-              title="UrbanGif"
-              live="https://josiehandeveloper.github.io/urban-gifs/"
-              client="https://github.com/josiehandeveloper/urban-gifs.git"
-              text="UrbanGifs is a fun application where users search within the
+          tech="React.js, CSS3, Node.js, Express, PostgreSQL"
+        />
+        <PortfolioItem
+          src="images/UrbanGif/search-macbook.png"
+          title="UrbanGif"
+          live="https://josiehandeveloper.github.io/urban-gifs/"
+          client="https://github.com/josiehandeveloper/urban-gifs.git"
+          text="UrbanGifs is a fun application where users search within the
                     UrbanDictionary database and the GIPHY database mashing both
                     results onto a search page! This application is for those
                     who like GIFS and Urban Dictionary, old and young users
                     alike will find this search bar as a fun and dynamic way to
                     search for words and GIFS."
-              tech="JavaScript ES6, jQuery, CSS3, HTML 5"
-            />
-          </ul>
-        </div>
+          tech="JavaScript ES6, jQuery, CSS3, HTML 5"
+        />
       </div>
     </div>
   );
--- src/Components/PortfolioItem/portfolioItem.js
+++ src/Components/PortfolioItem/portfolioItem.js
@@ -3,40 +3,30 @@ import "./PortfolioItem.css";
 
 function PortfolioItem(props) {
   return (
-    <div className="portfolio__container">
-      <div className="portfolio__wrapper">
-        <ul className="portfolio__items">
-          <li className="portfolio__item">
-            <div className="portfolio__item__box">
-              <div className="portfolio__item__pic-wrap">
-                <img
-                  className="portfolio__item__img"
-                  alt="Portfolio img"
-                  src={props.src}
-                />
-              </div>
-              <div className="portfolio__item__info">
-                <h4 className="portfolio__item__title">{props.title}</h4>
-                <h4 className="portfolio__item__links">
-                  <a href={props.live} target="_blank" rel="noreferrer">
-                    Live Site
-                  </a>
-                  <a href={props.client} target="_blank" rel="noreferrer">
-                    Client
-                  </a>
-                  <a href={props.server} target="_blank" rel="noreferrer">
-                    Server
-                  </a>
-                </h4>
-                <h5 className="portfolio__item__text">{props.text}</h5>
-                <h5 className="portfolio__item__tech">
-                  {" "}
-                  Technologies Used: {props.tech}
-                </h5>
-              </div>
-            </div>
-          </li>
-        </ul>
+    <div className="portfolio__item">
+      <img
+        className="portfolio__item__img"
+        alt="Portfolio img"
+        src={props.src}
+      />
+      <div className="portfolio__item__info">
+        <h4 className="portfolio__item__title">{props.title}</h4>
+        <h4 className="portfolio__item__links">
+          <a href={props.live} target="_blank" rel="noreferrer">
+            Live Site
+          </a>
+          <a href={props.client} target="_blank" rel="noreferrer">
+            Client
+          </a>
+          <a href={props.server} target="_blank" rel="noreferrer">
+            Server
+          </a>
+        </h4>
+        <h5 className="portfolio__item__text">{props.text}</h5>
+        <h5 className="portfolio__item__tech">
+          {" "}
+          Technologies Used: {props.tech}
+        </h5>
       </div>
     </div>
   );
